f44bb23224ff552cbe119884a5db92a4ee063cfa
chino <46995931+Chinosu@users.noreply.github.com>
2022-07-24 22:55:53 +1000

    Delete limbo.dart

--- limbo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:midi_player/midi_player.dart';
-
-class Limbo {
-
-  // notes:
-  //  at least 21, at most 108
-  static void playNote(MidiPlayer midiPlayer, int note, double velocity) =>
-      midiPlayer.playNote(note: note + 21, velocity: velocity);
-}
