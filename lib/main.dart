0db401700110eb11c16013ccfa31c322c929e4b5
chino <46995931+Chinosu@users.noreply.github.com>
2022-07-29 03:47:21 +1000

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,25 @@ class Pianista extends StatefulWidget {
 class _PianistaState extends State<Pianista> {
   // Variables
   final MidiPlayer _midiPlayer = MidiPlayer();
-  final tempoPlayer = AudioPlayer();
-  int _noteCounter = 0;
-  List<List<NoteOnEvent>> _noteOnEvents = [];
+  final _tempoPlayer = AudioPlayer();
+
+  final List<List<NoteOnEvent>> _noteOnEvents = [];
+
   bool _secondButtonState = false;
+
+  /// AnimatedList global key
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
   // Functions
 
   /// Play [note] on the piano at [velocity] (0 to 1.0, the maximum is used if omitted).
-  void playNote({required int note, double velocity = 1.0}) {
+  void _playNote({required int note, double velocity = 1.0}) {
     _midiPlayer.playNote(note: note - 0, velocity: velocity);
   }
 
   /// Open file dialog using [FilePicker] to select ONE midi file
   /// and return a sorted list of all [NoteOnEvent] from it.
-  Future<List<List<NoteOnEvent>>?> getMidiFile() async {
+  Future<List<List<NoteOnEvent>>?> _getMidiFile() async {
     // Open file browser dialog
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       allowMultiple: false,
@@ -71,53 +75,98 @@ class _PianistaState extends State<Pianista> {
           }
         }
       }
-      // List sorted by key
+      // Sort/Export map by key into list
       List<List<NoteOnEvent>> list = [];
       noteOnEvents.forEach((key, value) => list.add(value));
+
+      // Sort sub-list by pitch/tone/note (high to low)
+      for (var subList in list) {
+        subList.sort((a, b) => b.noteNumber.compareTo(a.noteNumber));
+      }
+
       return list;
     }
     return null;
   }
 
   /// Open [www.musescore.com] in external web browser.
-  Future<bool> launchMuseScore() async => await launchUrl(
-        Uri.parse('https://musescore.com'),
-        mode: LaunchMode.externalApplication,
-      );
+  Future<bool> _launchMuseScore() async {
+    return await launchUrl(
+      Uri.parse('https://musescore.com'),
+      mode: LaunchMode.externalApplication,
+    );
+  }
 
   /// Start the metronome according to the one of the five [tempo]
-  void startMetronome(String tempo) {
-    stopMetronome();
+  void _startMetronome(String tempo) {
+    _stopMetronome();
     switch (tempo) {
       case 'Adagio':
-        tempoPlayer.setAsset('assets/tempo/Adagio.mp3');
+        _tempoPlayer.setAsset('assets/tempo/Adagio.mp3');
         break;
       case 'Andante':
-        tempoPlayer.setAsset('assets/tempo/Andante.mp3');
+        _tempoPlayer.setAsset('assets/tempo/Andante.mp3');
         break;
       case 'Moderato':
-        tempoPlayer.setAsset('assets/tempo/Moderato.mp3');
+        _tempoPlayer.setAsset('assets/tempo/Moderato.mp3');
         break;
       case 'Vivace':
-        tempoPlayer.setAsset('assets/tempo/Vivace.mp3');
+        _tempoPlayer.setAsset('assets/tempo/Vivace.mp3');
         break;
       case 'Presto':
-        tempoPlayer.setAsset('assets/tempo/Presto.mp3');
+        _tempoPlayer.setAsset('assets/tempo/Presto.mp3');
         break;
     }
-    tempoPlayer.play();
+    _tempoPlayer.play();
   }
+
   /// Stop the metronome
-  void stopMetronome() => tempoPlayer.stop();
+  void _stopMetronome() => _tempoPlayer.stop();
 
+  /// Add to back-end [_noteOnEvents] AND notify front-end [_listKey]/[AnimatedList]
+  void _addItemToList({required List<NoteOnEvent> element, int? index}) {
+    index ??= _noteOnEvents.length;
+    _noteOnEvents.insert(index, element);
+    _listKey.currentState?.insertItem(index);
+  }
 
+  /// Remove from back-end [_noteOnEvents] AND notify front-end [_listKey]/[AnimatedList]
+  void _removeItemFromList({int? index}) {
+    index ??= _noteOnEvents.length - 1;
+    final List<NoteOnEvent> removedElement = _noteOnEvents[index];
+    _listKey.currentState?.removeItem(
+        index, (context, animation) => _buildWidget(removedElement, animation));
+    _noteOnEvents.removeAt(index);
+  }
+
+  /// Convert [List<NoteOnEvent>] to [List<Widget>]
+  Widget _buildWidget(
+      List<NoteOnEvent> removedElement, Animation<double> animation) {
+    return SizeTransition(
+      sizeFactor: animation,
+      axis: Axis.horizontal,
+      child: Card(
+        child: SizedBox(
+          height: 300,
+          width: 100,
+          child: ListTile(
+            leading: ElevatedButton(
+              onPressed: () {},
+              child: Text('test'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   // Functions (Overridden)
 
   @override
   void initState() {
     _midiPlayer.load('assets/midi/Steinway.sf2');
-    tempoPlayer.setLoopMode(LoopMode.one);
+    _tempoPlayer.setLoopMode(LoopMode.one);
+    _tempoPlayer.setVolume(0.2);
     super.initState();
   }
 
@@ -129,39 +178,55 @@ class _PianistaState extends State<Pianista> {
           title: Text("Test"),
         ),
         body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  var result = await getMidiFile();
-                  if (result != null) {
-                    _noteOnEvents = result;
-                    _noteCounter = 0;
-                    setState(() => _secondButtonState = true);
-                  }
-                  startMetronome('Vivace');
-                },
-                child: const Text("Load"),
-              ),
-              SizedBox(
-                width: 500,
-                height: 500,
-                child: ElevatedButton(
-                  onPressed: _secondButtonState
-                      ? () {
-                          for (final noteOnEvent
-                              in _noteOnEvents[_noteCounter++]) {
-                            playNote(note: noteOnEvent.noteNumber);
-                            if (_noteCounter >= _noteOnEvents.length) {
+          child: FittedBox(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    var result = await _getMidiFile();
+                    if (result != null) {
+                      while (_noteOnEvents.isNotEmpty) {
+                        _removeItemFromList();
+                      }
+                      for (var subResult in result) {
+                        _addItemToList(element: subResult);
+                      }
+                      setState(() => _secondButtonState = true);
+                    }
+                  },
+                  child: const Text("Load"),
+                ),
+                SizedBox(
+                  height: 700,
+                  width: 500,
+                  child: AnimatedList(
+                    physics: NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    key: _listKey,
+                    itemBuilder: (context, index, animation) =>
+                        _buildWidget(_noteOnEvents[index], animation),
+                  ),
+                ),
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: _secondButtonState
+                        ? () {
+                            for (final noteOnEvent in _noteOnEvents[0]) {
+                              _playNote(note: noteOnEvent.noteNumber);
+                            }
+                            _removeItemFromList(index: 0);
+                            if (_noteOnEvents.isEmpty) {
                               setState(() => _secondButtonState = false);
                             }
                           }
-                        }
-                      : null,
-                  child: const Text('Play'),
+                        : null,
+                    child: const Text('Play'),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
